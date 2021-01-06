a2873bc46b960d22df0c61dd4360e4d4677ed5dd
marcelosperalta <marcelosperalta@gmail.com>
2021-01-06 17:46:07 +0100

    pinterest

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const grids = document.querySelectorAll(".grid");
 const headings = document.querySelectorAll(".heading .wrapper .text");
 
 function enterScreen(index) {
+
     const grid = grids[index];
     const heading = headings[index];
     const gridColumns = grid.querySelectorAll(".column");
@@ -9,19 +10,42 @@ function enterScreen(index) {
     grid.classList.add("active");
 
     gridColumns.forEach(element => {
-        element.classList.remove('animate-before')
-    })
+        element.classList.remove('animate-before', 'animate-after');
+    });
+
+    heading.classList.remove('animate-before', 'animate-after');
+
 }
-function exitScreen(index, exitDelay) {}
 
-function setupAnimationCycle({ 
-    initalScreenIndex, 
-    timePerScreen, 
-    exitDelay
-}) {}
+function exitScreen(index, exitDelay) {
 
+    const grid = grids[index];
+    const heading = headings[index];
+    const gridColumns = grid.querySelectorAll(".column");
+
+    gridColumns.forEach(element => {
+        element.classList.add('animate-after');
+    });
+
+    heading.classList.add("animate-after");
+
+    setTimeout(() => {
+        grid.classList.remove("active");
+    }, exitDelay);
+
+}
+
+function setupAnimationCycle({ initialScreenIndex, timePerScreen, exitDelay}) {
+
+    enterScreen(initialScreenIndex);
+
+    setTimeout(() => {
+        exitScreen(initialScreenIndex, exitDelay);
+    }, timePerScreen);
+
+}
 setupAnimationCycle({ 
-    initalScreenIndex: 0,
+    initialScreenIndex: 0,
     timePerScreen: 2000, //ms
     exitDelay: 200 * 7 //ms
-})
+});
